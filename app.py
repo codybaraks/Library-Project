13f3901be4e84766e070e6d130142b663168153d
codybaraks <earvinbaraka@gmail.com>
2018-12-10 12:13:01 +0300

    updated!

--- app.py
+++ app.py
@@ -2,6 +2,8 @@ from flask import Flask, render_template, request, redirect, flash, url_for
 
 import mysql.connector as connector
 
+from  datetime import  date, timedelta
+
 db = connector.connect(host="localhost", user="root", passwd="root", database="library")
 
 app = Flask(__name__)
@@ -68,6 +70,31 @@ def book():
         flash("Your Successfuly saved!")
 
     return render_template('books.html')
+
+@app.route('/issue', methods=['POST', 'GET'])
+def issue():
+    if request.method == "POST":
+        book_id = request.form["book_id"]
+        borrower_id = request.form["borrower_id"]
+        date_borrowed= date.today()
+        expected_return= date.today()+timedelta(5)
+
+        sql="INSERT INTO `transactions`( `borrow_id`, `d_borowed`,  `expect_return_d`,`book_id`)" \
+            " VALUES (%s,%s,%s,%s)"
+        vals=(borrower_id, date_borrowed,expected_return, book_id)
+        cursor = db.cursor()
+        cursor.execute(sql, vals)
+        db.commit()
+        flash("Your Successfuly Issued Book!")
+
+    cursor = db.cursor()
+    sql = "SELECT `book_id`,`book_no` , `title` FROM `books`"
+    cursor.execute(sql)
+    books=cursor.fetchall()
+    sql2="SELECT `bid`,`phone`, `name` FROM `borrowers`"
+    cursor.execute(sql2)
+    people= cursor.fetchall()
+    return render_template('trans_form.html', books=books,people=people)
 
 @app.errorhandler(404)
 def error_page(e):

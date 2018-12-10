8dcc3b163887bbdf78baa2f57929c4573535b814
codybaraks <earvinbaraka@gmail.com>
2018-12-10 17:00:25 +0300

    Updated Project

--- app.py
+++ app.py
@@ -2,13 +2,14 @@ from flask import Flask, render_template, request, redirect, flash, url_for
 
 import mysql.connector as connector
 
-from  datetime import  date, timedelta
+from datetime import date, timedelta
 
 db = connector.connect(host="localhost", user="root", passwd="root", database="library")
 
 app = Flask(__name__)
 
 app.secret_key = 'jxbxjxdjdjdjddj'
+
 
 @app.route('/')
 def homes():
@@ -34,6 +35,7 @@ def forms():
         redirect(url_for('show_users'))
 
     return render_template('usr_form.html')
+
 
 @app.route('/show_users')
 def show_users():
@@ -61,6 +63,7 @@ def forms_bor():
         redirect(url_for('show_borrowers'))
 
     return render_template('brow_form.html')
+
 
 @app.route('/show_borrowers')
 def show_b():
@@ -90,6 +93,7 @@ def book():
 
     return render_template('books.html')
 
+
 @app.route('/show_books')
 def show_books():
     cursor = db.cursor()
@@ -104,12 +108,12 @@ def issue():
     if request.method == "POST":
         book_id = request.form["book_id"]
         borrower_id = request.form["borrower_id"]
-        date_borrowed= date.today()
-        expected_return= date.today()+timedelta(5)
+        date_borrowed = date.today()
+        expected_return = date.today() + timedelta(5)
 
-        sql="INSERT INTO `transactions`( `borrow_id`, `d_borowed`,  `expect_return_d`,`book_id`)" \
-            " VALUES (%s,%s,%s,%s)"
-        vals=(borrower_id, date_borrowed,expected_return, book_id)
+        sql = "INSERT INTO `transactions`( `borrow_id`, `d_borowed`,  `expect_return_d`,`book_id`)" \
+              " VALUES (%s,%s,%s,%s)"
+        vals = (borrower_id, date_borrowed, expected_return, book_id)
         cursor = db.cursor()
         cursor.execute(sql, vals)
         db.commit()
@@ -118,29 +122,29 @@ def issue():
     cursor = db.cursor()
     sql = "SELECT `book_id`,`book_no` , `title` FROM `books`"
     cursor.execute(sql)
-    books=cursor.fetchall()
-    sql2="SELECT `bid`,`phone`, `name` FROM `borrowers`"
+    books = cursor.fetchall()
+    sql2 = "SELECT `bid`,`phone`, `name` FROM `borrowers`"
     cursor.execute(sql2)
-    people= cursor.fetchall()
-    return render_template('trans_form.html', books=books,people=people)
+    people = cursor.fetchall()
+    return render_template('trans_form.html', books=books, people=people)
+
 
 # This section is for Button
 
 @app.route('/delete')
 def remove():
     cursor = db.cursor()
-    sql = "SELECT * FROM `borrowers`"
-    # UPDATE '' SET salary=500 WHERE id = 455
+    sql = "SELECT * FROM borrowers"
     cursor.execute(sql)
-    refugees = cursor.fetchall()
-    return render_template('remove.html', refugees=refugees)
+    borrowers = cursor.fetchall()
+    return render_template('show_borrower.html', borrowers=borrowers)
+
 
 @app.route('/del/<id>')
 def del_ref(id):
     cursor = db.cursor()
-    sql = "DELETE FROM borrowers WHERE id=%s"
+    sql = "DELETE FROM borrowers WHERE bid=%s"
     cursor.execute(sql, (id,))
-    # cursor.execute(sql)
     db.commit()
     flash('Borrower Successfully Removed ')
     return redirect(url_for('remove'))

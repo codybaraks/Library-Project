c6b4ee4bbb815e6dfdc89dd0c1d71fe60bcfbd6e
codybaraks <earvinbaraka@gmail.com>
2018-12-17 15:19:35 +0300

    validation Time

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ from flask import Flask, render_template, request, redirect, flash, url_for, ses
 import mysql.connector as connector
 
 from datetime import date, timedelta
-import time
+from myforms import *
 
 db = connector.connect(host="localhost", user="root", passwd="root", database="library")
 
@@ -22,25 +22,26 @@ def homes():
 # users uid, names, email, password, role
 @app.route('/form', methods=['POST', 'GET'])
 def forms():
-
     if session.get('names') == None:
         return redirect(url_for('login'))
-    if request.method == "POST":
-        name = request.form["name"]
-        email = request.form["email"]
-        role = request.form["role"]
-        password = request.form["password"]
+    form=UserRegForm()
+    if form.validate_on_submit():
+        if request.method == "POST":
+            name = request.form["name"]
+            email = request.form["email"]
+            role = request.form["role"]
+            password = request.form["password"]
 
-        print(name, email, password, role)
-        cursor = db.cursor()
-        sql = "INSERT INTO `users` (`name`, `email`, `password`, `role`) VALUES (%s,%s,%s,%s)"
-        val = (name, email, password, role)
-        cursor.execute(sql, val)
-        db.commit()
-        flash("Credentials Successfuly saved!")
-        redirect(url_for('show_users'))
+            print(name, email, password, role)
+            cursor = db.cursor()
+            sql = "INSERT INTO `users` (`name`, `email`, `password`, `role`) VALUES (%s,%s,%s,%s)"
+            val = (name, email, password, role)
+            cursor.execute(sql, val)
+            db.commit()
+            flash("Credentials Successfuly saved!")
+            redirect(url_for('show_users'))
 
-    return render_template('usr_form.html')
+    return render_template('usr_form.html', form=form)
 
 
 @app.route('/show_users')
@@ -90,22 +91,24 @@ def show_borrower():
 def book():
     if session.get('names') == None:
         return redirect(url_for('login'))
-    if request.method == "POST":
-        title = request.form["title"]
-        author = request.form["author"]
-        category = request.form["category"]
-        book_no = request.form["book_no"]
+    form=BookRegForm()
+    if form.validate_on_submit():
+        if request.method == "POST":
+            title = request.form["title"]
+            author = request.form["author"]
+            category = request.form["category"]
+            book_no = request.form["book_no"]
 
-        print(title, author, category, book_no)
-        cursor = db.cursor()
-        sql = "INSERT INTO `books`(`title`, `author`, `category`, `book_no`) VALUES (%s,%s,%s,%s)"
-        val = (title, author, category, book_no)
-        cursor.execute(sql, val)
-        db.commit()
-        flash("Your Successfuly saved!")
-        redirect(url_for('show_books'))
+            print(title, author, category, book_no)
+            cursor = db.cursor()
+            sql = "INSERT INTO `books`(`title`, `author`, `category`, `book_no`) VALUES (%s,%s,%s,%s)"
+            val = (title, author, category, book_no)
+            cursor.execute(sql, val)
+            db.commit()
+            flash("Your Successfuly saved!")
+            redirect(url_for('show_books'))
 
-    return render_template('books.html')
+    return render_template('books.html', form=form)
 
 
 @app.route('/show_books')
@@ -273,22 +276,24 @@ def returned(id):
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
+    loginForm=EmailPasswordForm()
     if request.method == 'GET':
-        return render_template('login.html')
-    email = request.form['email']
-    password = request.form['password']
-    cursor = db.cursor()
-    sql = "SELECT * FROM users WHERE email=%s AND password=%s"
-    vals = (email, password)
-    cursor.execute(sql, vals)
-    user = cursor.fetchone()
-    if len(user) > 1:
-        session['names'] = user[1]
-        session['role'] = user[4]
-        return redirect(url_for('homes'))
-    else:
-        flash('message', 'wrong username or password!')
-    return render_template('login.html')
+        return render_template('login.html', form=loginForm)
+    if loginForm.validate_on_submit():
+        email = request.form['email']
+        password = request.form['password']
+        cursor = db.cursor()
+        sql = "SELECT * FROM users WHERE email=%s AND password=%s"
+        vals = (email, password)
+        cursor.execute(sql, vals)
+        user = cursor.fetchone()
+        if user:
+            session['names'] = user[1]
+            session['role'] = user[4]
+            return redirect(url_for('homes'))
+        else:
+            flash('message', 'wrong username or password!')
+    return render_template('login.html', form=loginForm)
 
 
 @app.route('/logout')
@@ -297,7 +302,7 @@ def logout():
         return redirect(url_for('login'))
     session.pop('names')
     session.pop('role')
-    return render_template('login.html')
+    return redirect(url_for('login'))
 
 
 @app.errorhandler(404)
@@ -306,4 +311,4 @@ def error_page(e):
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, port=8888)
---
+++ myforms.py
@@ -0,0 +1,17 @@
+from flask_wtf import FlaskForm
+from wtforms import  StringField,PasswordField
+from wtforms.validators import DataRequired,Email, Length, Regexp
+
+class EmailPasswordForm(FlaskForm):
+    email = StringField('Email', validators=[DataRequired(),Email()])
+    password = PasswordField('Password', validators=[DataRequired(message="You must provide a password"),])
+
+class UserRegForm(FlaskForm):
+    name = StringField('Name', validators=[DataRequired(),Length(min=8, message="Your name is too short")])
+    email = StringField('Email', validators=[DataRequired(),Email(message="Invalid Emaill")])
+    password = PasswordField('Password', validators=[DataRequired(message="You must provide a password"), Length(min=6, message="Password Too short")])
+
+class BookRegForm(FlaskForm):
+    title=StringField('Title',validators=[DataRequired()])
+    author=StringField('Author',validators=[DataRequired(),Length(min=3, message="Author Too short")])
+    book_no=StringField('Book Number',validators=[DataRequired(), Regexp('^[A-Z](\/)[0-9]{4}$', message="Format should be B/xxxx")])
